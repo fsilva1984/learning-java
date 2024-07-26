becaa3e47fc1ce651aeff80374f6e2e8d62fb297
fsilva1984 <fsilva916@hotmail.com>
2024-07-25 22:35:00 -0300

    Hello World

---
+++ Fundamentos/src/00Strings/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+
+    public static void main(String[] args) {
+        
+        // Guardando uma cadeia de caracteres na str hi
+        String hi = "Hello, World!";
+        System.out.println(hi);
+    }
+}
