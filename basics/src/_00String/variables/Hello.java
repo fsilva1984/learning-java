f18b48c874a4bbab839936b698dd40d0db48ced9
fsilva1984 <fsilva916@hotmail.com>
2024-07-27 13:38:59 -0300

    organization

---
+++ basics/src/_00String/variables/Hello.java
@@ -0,0 +1,13 @@
+package _00String.variables;
+
+/**
+ * Hello
+ */
+public class Hello {
+
+  public static void main(String[] args) {
+    // Guardando uma cadeia de caracteres na str hi
+    String hi = "Hello, World!";
+    System.out.println(hi);
+  }
+}
---
+++ basics/src/_00String/variables/VariableDeclaration.java
@@ -0,0 +1,28 @@
+package _00String.variables;
+
+public class VariableDeclaration {
+  public static void main(String[] args) {
+    // variable naming
+
+    // pattern camelCase
+    String worldList = "Pão Baicon Leite Café Canela Queijo";
+    System.out.println(worldList);
+
+    // pattern positive naming
+    String _positivePattrer = "Pode começa com underscore";
+    System.out.println(_positivePattrer);
+
+    String $positivePattrer = "Pode começa com sifrão";
+    System.out.println($positivePattrer);
+
+    // pattern negative naming.
+    String negativePatternList = "Não começa com numeros, nem com nenhum outro simbulo\nque não seja o underscore ou sifrão, nem pode conter espaço";
+    System.out.println(negativePatternList);
+
+    // what the variable represents
+    String inputDate = "18/042023";
+    String fullName = "Flavio F Silva";
+    System.out.println(inputDate);
+    System.out.println(fullName);
+  }
+}
